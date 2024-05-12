dae5d5675ac104c3068e3cde51d94a19ca3ddb8f
ZoilaSantos <129453236+ZoilaSantos@users.noreply.github.com>
2024-05-12 14:28:12 -0400

    personal_finances_2023-2024.sql

--- personal_finances_2023-2024.sql
+++ personal_finances_2023-2024.sql
@@ -5,23 +5,14 @@ GO
 USE personal_finances;
 GO
 
-DROP TABLE IF EXISTS transactions
-CREATE TABLE transactions (
+DROP TABLE IF EXISTS [Personal Finances]
+CREATE TABLE [Personal Finances] (
 	entry_id int IDENTITY(2910,1) NOT NULL PRIMARY KEY,
 	entry_date date,
 	category varchar(40),
 	specific_expense varchar(40),
 	spent float
 );
-GO
-
-INSERT INTO transactions (entry_date, category, specific_expense, spent)
-SELECT 
-	CAST([Date] AS date),
-	[Category],
-	[Specific Expense],
-	[Spent]
-FROM [dbo].[Personal Finances] --Previously imported this table to the database from an Excel file I use to track my expenses
 GO
 
 
@@ -32,13 +23,12 @@ BEGIN TRY
 	SELECT
 		DATEPART(month,entry_date) [Month],
 		DATEPART(year,entry_date) [Year],
-		category [Category],
-		SUM(spent) [Total_Spent]
+		Category,
+		SUM(Spent) [Total_Spent]
 	INTO #monthly_expenses
-	FROM transactions
+	FROM [personal_finances].[dbo].[Personal Finances]
 	GROUP BY DATEPART(month,entry_date), DATEPART(year,entry_date), category 
 	ORDER BY [Year], [Month]
-
 
 	DROP TABLE IF EXISTS #monthly_categories
 	SELECT 
@@ -57,7 +47,6 @@ BEGIN TRY
 	FROM #monthly_categories
 	ORDER BY [Category];
 
-	
 	SET @SQLquery = '
 		SELECT *
 		INTO #monthly_expenses_pivot
@@ -74,12 +63,11 @@ BEGIN TRY
 		ISNULL([Car],0) [Car & Insurance],
 		ISNULL([Investing],0) [Roth IRA],
 		ISNULL([Groceries],0) [Groceries],
-		ISNULL([Pet],0) [Pet],
+		ISNULL([Newton],0) [Newton],
 		ISNULL([Miscellaneous/Fun],0) [Miscellaneous/Fun],
 		ISNULL([Emergency Fund],0) [From Emergency Fund],
 		ISNULL([From Savings],0) [From Savings],
-		ISNULL([Deposit],0) - ([Rent] + ISNULL([Investing],0) + ISNULL([Car],0) + ISNULL([Groceries],0) + ISNULL([Pet],0) + 
-					ISNULL([Miscellaneous/Fun],0)) [Savings: Rebuilding Emergency Fund]
+		ISNULL([Savings],0) [Savings]
 	FROM #monthly_expenses_pivot '
 
 	EXEC(@SQLquery);
@@ -91,12 +79,13 @@ END TRY
 
 BEGIN CATCH
 	SELECT
-		ERROR_NUMBER() AS ErrorNumber  
-		,ERROR_SEVERITY() AS ErrorSeverity  
-		,ERROR_STATE() AS ErrorState  
-		,ERROR_PROCEDURE() AS ErrorProcedure  
-		,ERROR_LINE() AS ErrorLine  
+		ERROR_NUMBER() AS ErrorNumber
 		,ERROR_MESSAGE() AS ErrorMessage
+		,ERROR_PROCEDURE() AS ErrorProcedure
+		,ERROR_STATE() AS ErrorState
+		,ERROR_SEVERITY() AS ErrorSeverity   
+		,ERROR_LINE() AS ErrorLine
+		,getdate()	
 END CATCH
 GO
 
@@ -105,43 +94,34 @@ CREATE PROCEDURE category_expenses (@month varchar(2), @year varchar(4))
 AS
 
 DECLARE @year_month varchar(6);
-SET @year_month = CONCAT(@year, @month);
+SET	@year_month = CONCAT(@year, @month);
 
-IF (@year_month NOT IN(SELECT DISTINCT CONCAT(CAST(DATEPART(year,entry_date) AS varchar(4)), CAST(DATEPART(month,entry_date) AS varchar(2)))
-			FROM personal_finances.dbo.transactions))
+IF (@year_month NOT IN(SELECT DISTINCT CONCAT(CAST(DATEPART(year,entry_date) AS varchar(4)), 
+											  CAST(DATEPART(month,entry_date) AS varchar(2)))
+						FROM dbo.[Personal Finances]))
 	BEGIN
 		PRINT('The month-year pair you are looking for is not available.')
 		RETURN
 	END
 
-DROP TABLE IF EXISTS #category_total
-SELECT 
-	[Category],
-	SUM(spent) [Total Spent]
-INTO #category_total
-FROM transactions
-WHERE 
-	DATEPART(month,entry_date) = @month
-	AND DATEPART(year,entry_date) = @year
-GROUP BY category;
-
-SELECT 
-	t1.[Category],
-	FORMAT(t1.[Total Spent],'C','en-US') [Total Spent],
-	FORMAT(t1.[Total Spent] / (SELECT [Total Spent] FROM #category_total WHERE [Category] = 'Deposit'),'P') [Percentage of Total Monthly Budget]
-FROM (
-	SELECT *
-	FROM #category_total
-	UNION
+	DROP TABLE IF EXISTS #category_total
 	SELECT 
-		'Savings',
-		(SELECT [Total Spent] FROM #category_total WHERE [Category] = 'Deposit') - SUM([Total Spent])
-	FROM #category_total
-	WHERE NOT [Category] = 'Deposit'
-) t1
-WHERE NOT t1.[Category] = 'Deposit'
+		[Category],
+		SUM(spent) [Total Spent]
+	INTO #category_total
+	FROM [dbo].[Personal Finances]
+	WHERE 
+		DATEPART(month,entry_date) = @month
+		AND DATEPART(year,entry_date) = @year
+	GROUP BY category;
 
-DROP TABLE IF EXISTS #category_total
+	SELECT
+		[Category],
+		FORMAT([Total Spent],'C','en-US') [Total Spent],
+		FORMAT([Total Spent] / 2800,'P') [Percentage of Total Monthly Budget]
+	FROM #category_total
+
+	DROP TABLE IF EXISTS #category_total
 GO
 
 EXEC dbo.category_expenses @month = '3', @year = '2024'
